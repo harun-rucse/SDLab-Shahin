a8fefd89f8ed44bb4e95da5a202011ed43e1ec7e
Shahin-Alam <shahinalam31416@gmail.com>
2021-12-06 08:47:47 +0600

    Check the output whether it matches with the output given here

--- StudentList.java
+++ StudentList.java
@@ -25,10 +25,10 @@ public class StudentList {
 					new InputStreamReader(
 							new FileInputStream("students.txt"))); 
 			String r = s.readLine();
-			System.out.println(r);
+			//System.out.println(r);
 			String i[] = r.split(",");	
 			Random x = new Random();
-				int y = x.nextInt();
+				int y = x.nextInt(i.length);
 					System.out.println(i[y]);
 			} catch (Exception e){} 
 			System.out.println("Data Loaded.");			
@@ -61,7 +61,7 @@ public class StudentList {
 			boolean done = false;
 			String t = args[0].substring(1);
 			for(int idx = 0; idx<i.length && !done; idx++) {
-				if(i[idx].equals(t)) {
+				if(i[idx].trim().equals(t)) {
 					System.out.println("We found it!");
 						done=true;
 				}
@@ -78,16 +78,17 @@ public class StudentList {
 							new FileInputStream("students.txt"))); 
 			String D = s.readLine();
 			char a[] = D.toCharArray();			
-			boolean in_word = false;
-			int count=0;
+			//boolean in_word = false;
+			int count=1;
 			for(char c:a) {
 				if(c ==' ') 
 				{
-					if (!in_word) {	count++; in_word =true;	}
-					else { in_word=false;}			
+					// if (!in_word) {	count++; in_word =true;	}
+					// else { in_word=false;}		
+					count++;
 				}
 			}
-			System.out.println(count +" word(s) found " + a.length);
+			System.out.println(count +" word(s) found ");
 			} catch (Exception e){} 
 			System.out.println("Data Loaded.");				
 		}
